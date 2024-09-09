d20af8272f7701a6781b07dd1ee9d90c9196a9a6
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-09-09 14:36:32 +0300

    WebService and Crypto created

---
+++ CryptoCrazyRxMVVM/Model/Crypto.swift
@@ -0,0 +1,13 @@
+//
+//  Crypto.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by Recep Taha Aydın on 9.09.2024.
+//
+
+import Foundation
+
+struct Crypto: Decodable {
+    let currency: String
+    let price: String
+}
