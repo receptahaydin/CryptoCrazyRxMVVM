0794c541dfe11c07529376c05e0ff788e7721f18
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-09-09 14:36:00 +0300

    Project created and TableView added

---
+++ CryptoCrazyRxMVVM/View/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by Recep Taha Aydın on 9.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "Crypto Currency"
+        content.secondaryText = "Crypto Price"
+        cell.contentConfiguration = content
+        return cell
+    }
+}
